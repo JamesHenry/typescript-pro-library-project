a4ab30b2c4ec6d3601798c11044723e7ddee7d56
James Henry <james@henry.sc>
2017-07-25 18:09:07 +0100

    New: Added isBoolean feature and tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,20 @@
-import lib from './index'
+import { isBoolean } from './index'
 
-test('it works', () => {
-  expect(true).toEqual(true)
-  console.log(lib)
+test('isBoolean should return true if the given value is an instance of Boolean', () => {
+  expect(isBoolean(new Boolean())).toEqual(true)
+})
+
+test('isBoolean should return true if the given value is the literal `true`', () => {
+  expect(isBoolean(true)).toEqual(true)
+})
+
+test('isBoolean should return true if the given value is the literal `false`', () => {
+  expect(isBoolean(false)).toEqual(true)
+})
+
+test('isBoolean should return false if the given value is any non-boolean value', () => {
+  expect(isBoolean('')).toEqual(false)
+  expect(isBoolean(undefined)).toEqual(false)
+  expect(isBoolean(null)).toEqual(false)
+  expect(isBoolean(123)).toEqual(false)
 })
